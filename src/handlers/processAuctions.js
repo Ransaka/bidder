8d16125a6a3add907c942bc9a8b20d88ad008235
ransaka <ransakaravihara@gmail.com>
2020-07-18 02:23:32 +0530

    error solving

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -2,6 +2,6 @@ import { getEndedAuctions } from '../lib/getEndedAuctions';
 
 async function processAuctions(event, context) {
     const auctionsToclose = await getEndedAuctions();
-    console.Console.log(auctionsToclose);
+    console.log(auctionsToclose);
 }
 export const handler = processAuctions;
